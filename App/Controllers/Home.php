aaa142b6f629b04488bb03e35bb650e72b3e5c73
code-architect <codearchitectdeveloper@gmail.com>
2016-11-03 22:39:35 +0530

    Added a new controller

---
+++ App/Controllers/Home.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+/**
+ * Home controller
+ *
+ * PHP Version 5.6
+ */
+
+class Home
+{
+
+    /**
+     * Show Index page
+     *
+     * @return void
+     */
+    public function index()
+    {
+        echo "Hello from index action of Home controller";
+    }
+
+
+
+
+}
