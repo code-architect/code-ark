86393165a5b54de3589445166b9ad6e7aafe7420
code-architect <codearchitectdeveloper@gmail.com>
2016-11-13 21:55:34 +0530

    Extraction database information into App/Config.php. Created a config file to store database information. Updating Database connection.

---
+++ App/Config.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ *
+ * PHP Version 5.6
+ */
+class Config
+{
+    /**
+     * Database Host
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database Name
+     */
+    const DB_NAME = 'code_ark';
+
+    /**
+     * Database User
+     */
+    const DB_USER = 'root';
+
+    /**
+     * Database Password
+     */
+    const DB_PASSWORD = '';
+}
--- Core/Model.php
+++ Core/Model.php
@@ -2,6 +2,7 @@
 
 namespace Core;
 
+use App\Config;
 use PDO;
 
 /**
@@ -21,15 +22,11 @@ abstract class Model
 
         if($db === null)
         {
-            $host       = 'localhost' ;
-            $dbname     = 'code_ark';
-            $username   = 'root';
-            $password   = '';
-
             try
             {
-                $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+                $dsn = "mysql:host=".Config::DB_HOST.";dbname=".Config::DB_NAME.";charset=utf8";
 
+                $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
                 return $db;
             }catch (\PDOException $e)
             {
